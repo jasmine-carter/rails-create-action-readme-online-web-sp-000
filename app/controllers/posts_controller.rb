3674059e6ae9fa29636b86c11a829fb8341dca2e
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-03 01:23:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,11 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new
-    post.title = params[:title]
-    post.description = params[:description]
-    post.save
-    recirect_to post_path(@post.id)
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    recirect_to post_path(@post)
   end
   # add create method here
 end
